41ea5e8bd7f2c87e2fdef0030c2a4b10b1684129
alekya arra <alekyarra07@gmail.com>
2024-06-27 23:03:59 +0530

    functionality to test one file

--- main.py
+++ main.py
@@ -57,10 +57,11 @@ def process_file(model, audio_file_name, out_file_name):
     predicted_speech = np.squeeze(predicted)
     predicted_speech = predicted_speech[384:384+len_orig]
     # write the file to target destination
+    print(out_file_name)
     sf.write(out_file_name, predicted_speech,fs)
       
 
-def process_folder(model, folder_name, new_folder_name):
+def process_folder(model, in_path):
     '''
     Function to find .wav files in the folder and subfolders of "folder_name",
     process each .wav file with an algorithm and write it back to disk in the 
@@ -80,34 +81,40 @@ def process_folder(model, folder_name, new_folder_name):
     '''
     
     # empty list for file and folder names
-    file_names = [];
-    directories = [];
-    new_directories = [];
+    # file_names = [];
+    # directories = [];
+    # new_directories = [];
     # walk through the directory
-    for root, dirs, files in os.walk(folder_name):
-        for file in files:
-            # look for .wav files
-            if file.endswith(".wav"):
+    # for root, dirs, files in os.walk(folder_name):
+    #     for file in files:
+    #         # look for .wav files
+    #         if file.endswith(".wav"):
                 # write paths and filenames to lists
-                file_names.append(file)
-                directories.append(root)
+
+    file  = in_path.split('/')[-1]
+    file = file.split('.')[0]
+    out_path = in_path.replace(file, file + '_processed')
+    out_file = out_path.split('/')[-1]
+    out_file = out_file.split('.')[0]
+                # file_names.append(file)
+                # directories.append(root)
                 # create new directory names
-                new_directories.append(root.replace(folder_name, new_folder_name))
+                # new_directories.append(root.replace(folder_name, new_folder_name))
                 # check if the new directory already exists, if not create it
-                if not os.path.exists(root.replace(folder_name, new_folder_name)):
-                    os.makedirs(root.replace(folder_name, new_folder_name))
+                # if not os.path.exists(root.replace(folder_name, new_folder_name)):
+                #     os.makedirs(root.replace(folder_name, new_folder_name))
     # iterate over all .wav files             
-    for idx in range(len(file_names)):
+    # for idx in range(len(file_names)):
         # process each file with the model
-        process_file(model, os.path.join(directories[idx],file_names[idx]), 
-                     os.path.join(new_directories[idx],file_names[idx]))
-        print(file_names[idx] + ' processed successfully!')
+    print(in_path, out_path)
+    process_file(model, in_path, out_path)
+    print(file + ' processed successfully!')
     
            
 
 
 # if __name__ == '__main__':
-def run_model(model, in_folder, out_folder):
+def run_model(model, in_path):
     # arguement parser for running directly from the command line
     # parser = argparse.ArgumentParser(description='data evaluation')
     # parser.add_argument('--in_folder', '-i',
@@ -129,6 +136,7 @@ def run_model(model, in_folder, out_folder):
     # load weights of the .h5 file
     modelClass.model.load_weights(model)
     # process the folder
-    process_folder(modelClass.model, in_folder, out_folder)
+    process_folder(modelClass.model, in_path)
+
 
 
--- main_1.py
+++ main_1.py
@@ -1,3 +1,3 @@
 from main import run_model
 
-run_model("./pretrained_model/model.h5", "test", "./output_test_2")
+run_model("./pretrained_model/model.h5", "/home/ap/Desktop/Workspace/model_denoising/DTLN/test/83-11691-0028.wav")
